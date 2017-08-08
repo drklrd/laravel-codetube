9bedf132480037bafa476a2d1b521427c7a29166
drklrd <srvbhattarai@gmail.com>
2017-08-08 23:19:37 +0545

    video-selection vue component

---
+++ resources/assets/js/components/VideoUpload.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Upload</div>
+
+                    <div class="panel-body">
+                        <input type="file" name="video" id="video" @change="fileInputChange" v-if="!uploading" >
+                        <div id="video-form" v-if="uploading && !failed">
+                            Form
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data(){
+            return {
+                uploading : false,
+                uploadingComplete : false,
+                failed : false
+
+            }
+        },
+        methods : {
+            fileInputChange(){
+                console.log('file change.')
+
+                this.uploading = true;
+            }
+        },
+
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
